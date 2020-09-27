0b901ab2ca92e05e8d81a60d2de3db0624f1f7fe
Jim Oconnell <jimoconnell@Jims-MacBook.home>
2020-09-27 14:02:48 -0400

    Reactive WebClient

--- movie-catalog-service/src/main/java/com/joc/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/joc/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,19 +1,48 @@
 package com.joc.moviecatalogservice.resources;
 
 import com.joc.moviecatalogservice.models.CatalogItem;
+import com.joc.moviecatalogservice.models.Movie;
+import com.joc.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(new CatalogItem("Good Will Hunting", "Went to see about a girl", 10));
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+
+        return ratings.stream().map(rating -> {
+//          Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            return new CatalogItem(movie.getName(), "Went to see about a girl", rating.getRating());
+        })
+        .collect(Collectors.toList());
     }
 }
