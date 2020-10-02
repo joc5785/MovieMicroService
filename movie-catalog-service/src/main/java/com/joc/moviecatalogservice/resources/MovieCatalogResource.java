e23038ef447b3d01861784edbd6895db0beb48b6
Jim Oconnell <jimoconnell@Jims-MacBook.home>
2020-10-01 23:04:29 -0400

    Service discovery

--- movie-catalog-service/src/main/java/com/joc/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/joc/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.joc.moviecatalogservice.resources;
 import com.joc.moviecatalogservice.models.CatalogItem;
 import com.joc.moviecatalogservice.models.Movie;
 import com.joc.moviecatalogservice.models.Rating;
+import com.joc.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,21 +29,23 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
-//          Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
+        return userRating.getUserRating().stream().map(rating -> {
+            // For each movie ID, call movie info service and get details
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            // Put them all together
             return new CatalogItem(movie.getName(), "Went to see about a girl", rating.getRating());
         })
         .collect(Collectors.toList());
+
+        /* WebClient Example:
+           Movie movie = webClientBuilder.build()
+                   .get()
+                   .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                   .retrieve()
+                   .bodyToMono(Movie.class)
+                   .block();
+         */
     }
 }
